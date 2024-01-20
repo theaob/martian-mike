8db3cb43e925ac3dbd2819282fd6e2506419cb7b
Onur Baykal <theaob@gmail.com>
2024-01-20 15:51:51 +0300

    fix movement code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,26 @@
 extends CharacterBody2D
 
-@export var gravity = 400;
-@export var speed = 125;
-@export var jump_velocity = 400.0
+@export var gravity = 400
+@export var speed = 150
+@export var jump_velocity = 250
 
-const GRAVITY_CAP = 500
+const TERMINAL_VELOCITY = 500
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	
-	if !is_on_floor(): 
-		velocity.y += delta * gravity
-		if velocity.y >= GRAVITY_CAP:
-			velocity.y = GRAVITY_CAP
-	
 	# left -1 right 1 still 0
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
-	
 	var jumpPressed = Input.is_action_just_pressed("jump")
 	
-	if jumpPressed: velocity.y += -jump_velocity
+	if not is_on_floor(): 
+		velocity.y += delta * gravity
+		clamp(velocity.y, TERMINAL_VELOCITY, -TERMINAL_VELOCITY)
+		
+	# jump only if on floor and input received
+	if is_on_floor() && jumpPressed: velocity.y += -jump_velocity
 	
+	velocity.x = direction * speed
 	
 	move_and_slide()
