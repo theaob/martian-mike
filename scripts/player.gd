bb1c1cbadabe9b322598556d44d1383f312f7c2f
Onur Baykal <theaob@gmail.com>
2024-01-20 15:41:27 +0300

    add player movement code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var gravity = 400;
+@export var speed = 125;
+@export var jump_velocity = 400.0
+
+const GRAVITY_CAP = 500
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	var rightPressed = Input.is_action_pressed("move_right")
-	var leftPressed = Input.is_action_pressed("move_left")
-	var jumpPressed = Input.is_action_pressed("jump")
-
-	if rightPressed: animated_sprite.play("run")
-	if leftPressed: animated_sprite.play("run")
-	if jumpPressed: animated_sprite.play("jump")
+func _physics_process(delta):
+	
+	if !is_on_floor(): 
+		velocity.y += delta * gravity
+		if velocity.y >= GRAVITY_CAP:
+			velocity.y = GRAVITY_CAP
+	
+	# left -1 right 1 still 0
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	var jumpPressed = Input.is_action_just_pressed("jump")
+	
+	if jumpPressed: velocity.y += -jump_velocity
+	
+	
+	move_and_slide()
