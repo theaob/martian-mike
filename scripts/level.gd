c9e1a6db163be021331384ac5fc9825659748f9c
Onur Baykal <theaob@gmail.com>
2024-01-20 16:15:41 +0300

    add death zone

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+
 func _process(_delta):
 	var quitPressed = Input.is_action_pressed("quit")
 	var resetPressed = Input.is_action_pressed("reset")
@@ -8,3 +10,8 @@ func _process(_delta):
 	if resetPressed: get_tree().reload_current_scene()
 	
 	pass
+
+
+func _on_death_zone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
